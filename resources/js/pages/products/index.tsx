ded37e897679212aa4040fb647c6b42f5c16f0a6
ilyasmaidi <maidiilias@gmail.com>
2025-04-18 09:05:27 -0700

    + table products

--- resources/js/pages/products/index.tsx
+++ resources/js/pages/products/index.tsx
@@ -10,25 +10,28 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-export default function ProductsListing() {
+export default function Index() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Dashboard" />
+            <Head title="Manage Products" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                </div>
-                <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                </div>
+                <table className='w-full table-auto'>
+                    <thead>
+                        <tr>
+                             <th>#</th>
+                             <th>name</th>
+                             <th>description</th>
+                             <th>Price</th>
+                             <th>Featred Image</th>
+                             <th>Create Date</th>
+                             <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+
+                    </tbody>
+                </table>
             </div>
         </AppLayout>
     );
